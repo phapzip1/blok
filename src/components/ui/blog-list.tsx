4655db7e35931c80c6858dd57da855e25d54a57a
Nguyen Phap <phapdeptrai2k2@gmail.com>
2025-12-18 21:56:17 +0700

    added ceo utility and improved get posts function

--- src/components/ui/blog-list.tsx
+++ src/components/ui/blog-list.tsx
@@ -1,25 +1,22 @@
-import { files } from "@/lib/blogs";
+import { getAllPosts } from "@/lib/blogs";
 import DecoratedLink from "./decorated-link";
 
 const BlogList: React.FC = async () => {
-    const f = await files;
-    const mdxes = await Promise.all(f.map(name => import(`@/content/${name}.mdx`)));
-
+    const posts = await getAllPosts();
+    
     return (
         <ul className="grid grid-cols-2 gap-x-2 gap-y-4">
-            {mdxes.map(({ frontmatter }, index) => {
-                const { title, subtitle, description } = frontmatter;
-
+            {posts.map(({ metadata, slug }) => {
                 return (
-                    <li key={index} className="rounded-md p-4 bg-background">
+                    <li key={slug} className="rounded-md p-4 bg-background">
                         <DecoratedLink
-                            href={`/post/${f[index]}`}
+                            href={`/post/${slug}`}
                             className="block text-lg font-semibold w-fit"
                         >
-                            {title}
+                            {metadata.title}
                         </DecoratedLink>
-                        <span className="block text-lg font-semibold text-muted mb-2">{subtitle}</span>
-                        <span>{description}</span>
+                        <span className="block text-lg font-semibold text-muted mb-2">{metadata.subtitle}</span>
+                        <span>{metadata.description}</span>
                     </li>
                 );
             })}
